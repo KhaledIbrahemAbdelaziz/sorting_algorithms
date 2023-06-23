d98cc5b4bfd8cb684d9ee078c8f09b2f9f5b3659
your name <you@example.com>
2023-06-23 15:37:35 -0700

    Task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,7 @@
 #include "sort.h"
+
+int *lomute_part(int *arr, size_t size, int low, int high);
+
 /**
  * quick_sort - sorts an array of integers
  * in ascending order using the Quick sort algorithm.
@@ -9,3 +12,16 @@
 void quick_sort(int *array, size_t size)
 {
 }
+
+/**
+ * lomute_part - partition and sorting array.
+ * @arr: The array.
+ * @size: The size of the array.
+ * @low: the address of the beginning.
+ * @high: the address of the ending.
+ * Return: sorted array.
+ */
+int *lomute_part(int *arr, size_t size, int low, int high)
+{
+	int pivot = arr[size - 1];
+	int j = 0
