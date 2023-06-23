ac0392420eb1ef7e8e9116af9f0ebb3e4623562a
Bigizic <olalekanisaac75@gmail.com>
2023-06-23 00:26:25 +0000

    Add main

---
+++ main/0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers
+ * in ascending order.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * Return: nothing to return.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int tmp_arr;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp_arr = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp_arr;
+				print_array(array, size);
+			}
+			else
+				continue;
+		}
+	}
+	/*for (i = 0; i < size; i++)
+		printf("%d, ", array[i]);*/
+	printf("\n");
+}
