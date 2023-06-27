714f473ff26e2bff59efdca3f6efe614e6e907bf
your name <you@example.com>
2023-06-27 15:21:37 -0700

    Task 102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,20 +10,17 @@
 void counting_sort(int *array, size_t size)
 {
 	int *sorted = NULL, *counts = NULL;
-	int j = 0, max = 0;
-	size_t i = 0;
+	int j, max = 0;
+	size_t i;
 
 	if (array == NULL || size < 2)
 		return;
 	sorted = malloc(sizeof(int) * size);
-	sorted = malloc(sizeof(int) * size);
 	if (sorted == NULL)
-	{
 		return;
-	}
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (array[i] > max)
+		if(array[i] > max)
 			max = array[i];
 	}
 	counts = malloc(sizeof(int) * (max + 1));
@@ -32,18 +29,13 @@ void counting_sort(int *array, size_t size)
 		free(sorted);
 		return;
 	}
-	for (; j <= max; j++)
+	for (j = 0; j < (max + 1); j++)
 		counts[j] = 0;
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 		counts[array[i]]++;
-	for (; j <= max; j++)
-		counts[j] = counts[j] + counts[j - 1];
-	print_array(counts, max +1);
-	for (; i < size; i++)
-	{
-		sorted[--counts[array[i]]] = array[i];
-		counts[array[i]]--;
-	}
-	free(sorted);
-	free(counts);
+	for (j = 0; j < (max + 1); j++)
+		counts[j] += counts[j - 1];
+	print_array(counts, max + 1);
+	/*frcountsee(sorted);*/
+	/*free(counts);*/
 }
