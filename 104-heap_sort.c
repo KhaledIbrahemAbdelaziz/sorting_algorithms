ed209408424725c08bac91b08cf9bde19c688cbc
Bigizic <olalekanisaac75@gmail.com>
2023-07-03 18:56:22 +0000

    Update heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,8 +1,5 @@
 #include "sort.h"
 
-void heapify(int *arr, size_t size, int i);
-void swapper(int *a, int *b);
-
 
 /**
  * swapper - swaps two elements of an array
@@ -23,34 +20,37 @@ void swapper(int *a, int *b)
 
 
 /**
- * heapify - a subtree rooted with node i
+ * heapify - a subtree rooted with node index, also the implementation
+ * of the heap sort algorithm
  *
  * @array: array containing elements
  *
  * @size: size of array
  *
- * @i: index in array
+ * @index: index in array
+ *
+ * @size_cp: copies the actual size of the array
  *
  * Return: void
  */
 
-void heapify(int *array, size_t size, int i)
+void heapify(int *array, int size, int index, int size_cp)
 {
-	int max_int = i;
-	size_t left = 2 * i + 1;
-	size_t right = 2 * i + 2;
+	int max_int = index;
+	int left = 2 * index + 1;
+	int right = 2 * index + 2;
 
-	if ((left < size) && (array[left] > array[max_int]))
+	if (left < size && array[left] > array[max_int])
 		max_int = left;
 
-	if ((right < size) && (array[right] > array[max_int]))
+	if (right < size && array[right] > array[max_int])
 		max_int = right;
 
-	if (max_int != i)
+	if (max_int != index)
 	{
-		swapper(&array[i], &array[max_int]);
-		heapify(array, size, max_int);
-		print_array(array, size);
+		swapper(&array[index], &array[max_int]);
+		print_array(array, size_cp);
+		heapify(array, size, max_int, size_cp);
 	}
 
 }
@@ -58,22 +58,28 @@ void heapify(int *array, size_t size, int i)
 
 /**
  * heap_sort - sorts an array of integers
+ *
  * in ascending order using the Heap sort algorithm.
+ *
  * @array: the array.
+ *
  * @size: the size of the array.
+ *
  * Return: nothing.
  */
 void heap_sort(int *array, size_t size)
 {
 	int i;
+	size_t size_cp = size;
 
-	for (i = (size / 2) - 1; i >= 0; i--)
-		heapify(array, size, i);
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size_cp);
 
 	for (i = size - 1; i >= 0; i--)
 	{
 		swapper(&array[0], &array[i]);
-		print_array(array, size);
-		heapify(array, i, 0);
+		if (i > 0)
+			print_array(array, size);
+		heapify(array, i, 0, size_cp);
 	}
 }
