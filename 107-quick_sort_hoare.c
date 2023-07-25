46e8a7c9365c91703e97dcce3f89aa107b620f80
Bigizic <olalekanisaac75@gmail.com>
2023-07-25 02:21:12 +0000

    Update quick_sort_hoare.c

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,106 @@
+#include "sort.h"
+
+/**
+ * swap_values - swaps values from an array of integers
+ *
+ * @array: the array of ints
+ *
+ * @i1: index of first value
+ *
+ * @i2: index of second value
+ *
+ * Return: new array with swapped values
+ */
+
+void swap_values(int **array, ssize_t i1, ssize_t i2)
+{
+	int tmp;
+
+	tmp = (*array)[i1];
+	(*array)[i1] = (*array)[i2];
+	(*array)[i2] = tmp;
+}
+
+/**
+ * partition - partitions for quicksort using the Hoare scheme
+ *
+ * @array: the array to sort
+ *
+ * @lo: the lowest index of the partition
+ *
+ * @hi: the highest index of the partition
+ *
+ * @size: size of the array
+ *
+ * Return: index of the partition
+ */
+size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
+{
+	int pivot;
+
+	pivot = array[hi];
+
+	while (lo <= hi)
+	{
+		while (array[lo] < pivot)
+			lo++;
+		while (array[hi] > pivot)
+			hi--;
+		if (lo <= hi)
+		{
+			if (lo != hi)
+			{
+				swap_values(&array, lo, hi);
+				print_array(array, size);
+			}
+			lo++;
+			hi--;
+		}
+	}
+	return (hi);
+}
+
+/**
+ * _quick_sort - partitions the array, then sorts each partition
+ *
+ * @array: the array to be sorted
+ *
+ * @lo: the lowest index of the partition to sort
+ *
+ * @hi: the highest index of the partition to sort
+ *
+ * @size: size of the array
+ */
+
+void _quick_sort(int *array, ssize_t lo, ssize_t hi, size_t size)
+{
+	ssize_t pivot;
+
+	if (lo < hi)
+	{
+		pivot = partition(array, lo, hi, size);
+		_quick_sort(array, lo, pivot, size);
+		_quick_sort(array, pivot + 1, hi, size);
+	}
+}
+
+
+
+/**
+ * quick_sort_hoare - implementation of the quick sort hoare algo
+ *
+ * @array: the array to be used
+ *
+ * @size: the size of the array
+ */
+
+void quick_sort_hoare(int *array, size_t size)
+{
+	ssize_t lo = 0;
+	ssize_t hi = (size - 1);
+
+	if (!array || size < 2)
+		return;
+
+	_quick_sort(array, lo, hi, size);
+}
